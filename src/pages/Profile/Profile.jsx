b28848aa24e13e6f4a0ede6f9634ccb06502c4eb
Fayzuloh <abdusaliqofayzuloh@gmail.com>
2026-05-06 15:07:50 +0500

    kids academy

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -14,16 +14,18 @@ function Profile() {
   async function getProfile() {
     try{
       const res = await axios.get("https://kindergarten-4d40e-default-rtdb.firebaseio.com/Profil.json")
-      const data = await res.data.find((profile) => profile.profileId == student?.profileId)
-      setProfile(data)
+      // const data = await res.data.find((profile) => profile.profileId == student?.profileId)
+      console.log(res.data)
+      const profiles = await [...res.data]
+      console.log(profiles)
+      // setProfile(data)
     }catch(err){
       console.log(err)
     }
   }
   async function patchProfile() {
     try {
-
-      const res = await axios.patch(`https://kindergarten-4d40e-default-rtdb.firebaseio.com/Profil/${profile.firebaseKey}.json`,
+      const res = await axios.patch(`https://kindergarten-4d40e-default-rtdb.firebaseio.com/Profil/${profile?.profileId}.json`,
         {
           avatar: avatarInput.current.value,
           childName: nameInput.current.value,
@@ -46,7 +48,7 @@ function Profile() {
       <div className="conteyner hero__wraper-profile">
         <img width={355} src={profile?.avatar} alt="" className="hero_img" />
         <div className="hero__inner">
-         <h1 className="hero_title">{profile?.name} Profile</h1>
+         <h1 className="hero_title">{profile?.childName} Profile</h1>
          <p className="hero_text">this is your profile</p>
         </div>
       </div>
@@ -59,25 +61,25 @@ function Profile() {
         }} className="profile__form">
           <div className="profile__content">
             <label htmlFor="childName" className="profile_label">Change your child name:</label>
-            <input id='childName' type="text" className="profile_input" ref={nameInput}/>
+            <input id='childName' type="text" className="profile_input" value={profile?.childName} ref={nameInput}/>
           </div>
           <div className="profile__content">
             <label htmlFor="childAge" className="profile_label">Change your child age:</label>
-            <input id='childAge' type="number" className="profile_input" ref={ageInput}/>
+            <input id='childAge' type="number" className="profile_input" value={profile?.age} ref={ageInput}/>
           </div>
           <div className="profile__content">
             <div className="profile__inner">
               <label htmlFor="childPhoto" className="profile_label">Change your child photo url:</label>
-              <input id='childPhoto' type="text" className="profile_input" ref={avatarInput}/>
+              <input id='childPhoto' type="text" className="profile_input" value={profile?.avatar} ref={avatarInput}/>
             </div>
           </div>
           <div className="profile__content">
             <label htmlFor="phoneNumber" className="profile_label">Change your phone number:</label>
-            <input id='phoneNumber' type="number" className="profile_input" ref={phoneInput}/>
+            <input id='phoneNumber' type="number" className="profile_input" value={profile?.phone} ref={phoneInput}/>
           </div>
           <div className="profile__content">
             <label htmlFor="email" className="profile_label">Change your email:</label>
-            <input id='email' type="email" className="profile_input" ref={emailInput}/>
+            <input id='email' type="email" className="profile_input" value={profile?.email} ref={emailInput}/>
           </div>
           <button className="profil_btn">submit</button>
         </form>
