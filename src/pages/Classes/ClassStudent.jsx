3a29b3fc0546b904ae1738a9bfa469a11a62259c
Fayzuloh <abdusaliqofayzuloh@gmail.com>
2026-05-07 23:26:57 +0500

    kids academy

--- src/pages/Classes/ClassStudent.jsx
+++ src/pages/Classes/ClassStudent.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import './ClassStudent.css'
 
 function ClassStudent() {
-  return (<>
-  
-  </>)
+  const [classesData, setClassesData] = useState([])
+  const studentObject = JSON.parse(localStorage.getItem("studentObject"))
+
+  async function getClasses() {
+    try{
+      const res = await axios.get('https://kindergarten-4d40e-default-rtdb.firebaseio.com/Classes.json')
+      const classes = await Object.values(res.data)
+      const data = await classes.find((group) => group.id == studentObject?.id)
+      console.log(data)
+      setClassesData(data.students)
+    }catch(err){
+      console.log(err.message)
+    }
+  }
+  useEffect(() => {
+    getClasses()
+  }, [])
+
+  return (<main className='site__main'>
+        <section className='classes-card'>
+         <div className="conteyner">
+            <div className='student-list'>
+              {
+                classesData.map((student, index) => (
+                  <div key={student.id} className='student-item'>
+                    <div className='student-item__left'>
+                      <div>
+                        <h3>{student.name}</h3>
+                        <p>{studentObject?.email}</p>
+                      </div>
+                    </div>
+                    <div className='student-item__right'>
+                      <span className='student-coin'>{student.age} years old</span>
+                      <span className='student-chip'>{index + 1}</span>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+         </div> 
+        </section>
+    </main>)
 }
 
 export default ClassStudent
--- src/pages/Shop/ShopStudent.jsx
+++ src/pages/Shop/ShopStudent.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 function ShopStudent() {
   const [score, setScore] = useState({})
+  const [products, setProducts] = useState([])
   const student = JSON.parse(localStorage.getItem("studentObject"))
 
   async function getCoins(){
@@ -17,16 +18,40 @@ function ShopStudent() {
       console.log(err.message)
     }
   }
+  async function getProducts(){
+    try{
+      const res = await axios.get("https://kindergarten-4d40e-default-rtdb.firebaseio.com/Product.json")
+      const data = await Object.values(res.data)
+      console.log(data)
+      setProducts(data)
+    }catch(err){
+      console.log(err)
+    }
+  }
   useEffect(() => {
     getCoins()
+    getProducts()
   },[])
 
   return (<main className='site__main'>
-    <section className="hero">
+    <section className="hero shop">
       <div className="conteyner">
         <div className="hero__wraper-shop">
-
+          
         </div>
+      </div>
+    </section>
+    <section className="products">
+      <div className="conteyner">
+        <ul className="products__list">
+          {
+            products.map((product) => (<li key={product.id} className="products__item">
+            <h3 className="products_subtitle">{product.category}</h3>
+            <p className="products_text">{product.name}</p>
+            <strong className="prducts_strong">{product.price}</strong>
+          </li>))
+          }
+        </ul>
       </div>
     </section>
   </main>)
