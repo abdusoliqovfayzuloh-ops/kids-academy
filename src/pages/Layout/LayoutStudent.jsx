cdc4f89af6871cb85a7a18ceafa6ecb78b63e377
Fayzuloh <abdusaliqofayzuloh@gmail.com>
2026-05-13 05:00:45 +0500

    kids academy

--- src/pages/Layout/LayoutStudent.jsx
+++ src/pages/Layout/LayoutStudent.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Header from '../../components/Header/HeaderStudent'
 import { Outlet } from 'react-router-dom'
+import "./LayoutStudent.css"
 
 function LayoutStudent() {
   return (<>
